73ccf6a1409c7312501950645ce4f583976aa9a8
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2020-03-25 09:45:44 -0300

    add frontend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -9,7 +9,7 @@ const routes = express.Router();
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.store);
 
-routes.post("/session", SessionController.store);
+routes.post("/sessions", SessionController.store);
 
 routes.get("/profile", ProfileController.index);
 
--- src/database/connection.js
+++
@@ -1,6 +0,0 @@
-const knex = require("knex");
-const configuration = require("../../knexfile");
-
-const connection = knex(configuration.development);
-
-module.exports = connection;
